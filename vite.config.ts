08f5c8cf6513e2b947babe2103c8531af448f470
Adnan Mahmood <msaadnanil18@gmail.com>
2025-09-09 15:56:24 +0530

    Integrated email service

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 3000,
-    allowedHosts: true,
+  // server: {
+  //   host: "::",
+  //   port: 3000,
+  //   allowedHosts: true,
+  // },
+  plugins: [react(), viteSingleFile()],
+  build: {
+    target: "esnext",
+    minify: true,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean
-  ),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
